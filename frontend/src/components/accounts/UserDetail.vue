db5c9d3fbfba81efa6c6c72dea33dd9696df3a73
kimtaehyeong93 <bobosara@naver.com>
2020-07-21 00:00:19 +0900

    0720 | start vue pjt

---
+++ frontend/src/components/accounts/UserDetail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'UserDetail',
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/views/accounts/SignupView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>
+      <label for="nickname">nickname : </label>
+      <input type="text" id="nickname">
+    </div> 
+     <div>
+      <label for="email">email : </label>
+      <input type="text" id="email">
+    </div>
+    <div>
+      <label for="password1">nickname : </label>
+      <input type="password" id="password1">
+    </div>
+    <div>
+      <label for="password2">nickname : </label>
+      <input type="password" id="password">
+    </div>
+    <button>signup</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'SignupView',
+}
+</script>
+
+<style>
+
+</style>
