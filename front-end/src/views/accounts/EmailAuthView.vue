ef5ef081e7097983ffa73a650f3b0776f6d13866
차보람 <ckqhfka4520@naver.com>
2020-07-22 02:24:14 +0900

    0722 | Create EmailAuthPage

--- front-end/src/views/accounts/EmailAuthView.vue
+++ front-end/src/views/accounts/EmailAuthView.vue
@@ -1,55 +1,31 @@
 <template>
-  <div>
-   
-    <b-container fluid>
-      <b-row class="my-1">
-        <b-col sm="3"><!--길이-->
-          <label for="input-userid">E-mail</label>
-        </b-col>
-        <b-col sm="6">
-          <b-form-input 
-            id="input-userid" 
-            v-model="userid"
-            :state="useridState"
-            aria-describedby="input-userid-help input-userid-feedback"
-            placeholder=""
-            trim
-            >
-          </b-form-input>
-          <b-form-invalid-feedback id="userid-feedback">
-            아이디를 입력하세요(영어만 가능, 최소8글자)
-          </b-form-invalid-feedback>
-        </b-col>
-        <b-col sm="3">
-          <b-button variant="primary">확인</b-button>
-        </b-col>
-      </b-row>  
-
-      <b-row class="my-1">
-        <b-col sm="3"><!--길이-->
-          <label for="input-none">인증코드</label>
-        </b-col>
-        <b-col sm="6">
-          <b-form-input type="password" id="password" aria-describedby="password-feedback" v-model="password" :state="passwordValid"></b-form-input>
-          <b-form-invalid-feedback id="password-feedback">
-          영문자와 숫자를 포함해 8글자 이상으로 입력하세요. </b-form-invalid-feedback>
-        </b-col>
-        <b-col sm="3">
-          <b-button variant="primary">확인</b-button>
-        </b-col>
-      </b-row>
-    </b-container>
-    <b-button variant="primary">확인</b-button>
-
+  <div id="email-auth">
+    <h2>이메일 확인</h2>
+    <hr>
+     <div class="inner-center">회원 가입을 위해서는 E-mail 인증이 필요합니다.<br>
+    입력한 이메일로 인증코드를 보냅니다.</div>
+    <EmailAuth/>
   </div>
 </template>
 
 <script>
-  export default {
-    
-  }
+import EmailAuth from "@/components/accounts/EmailAuth.vue"
+export default {
+    name: 'EmailAuthView',
+    components: {
+      EmailAuth,
+    }
+}
 </script>
 
 <style>
-
+  #email-auth {
+    width: 50%;
+    display: block;
+    margin: 0px auto;
+  }
+  .inner-center {
+    text-align: center;
+    margin: 2em;
+  }
 </style>
