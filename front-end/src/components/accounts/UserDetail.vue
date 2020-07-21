5ebf81b43c6e7a166ee123d75056ceeb26bcf9ab
EunJeong <kkwak96@gmail.com>
2020-07-21 20:18:52 +0900

    0721 | Update components/UserDetail.vue #S03P12A201-51

--- front-end/src/components/accounts/UserDetail.vue
+++ front-end/src/components/accounts/UserDetail.vue
@@ -1,27 +1,99 @@
 <template>
-  <div id="app">
-    <h1>개인정보 조회</h1>
-    <h3>아이디 {{ user.username }}</h3>
-    <h3>이메일 {{ user.email }} </h3>
-    <h3>비밀번호</h3>
-    <h3>비밀번호 재입력</h3>
-    <h3>닉네임</h3>
-    <h3>프로필 사진</h3>
-    <h3>소개글</h3>
-    <button>수정</button>
-    <button>탈퇴</button>
+  <div id="userinfo">
+    <b-container fluid="lg" id="userinfo-container">
+    <b-row align-v="center">
+      <b-col sm="3">아이디</b-col>
+      <b-col sm="9">
+        user.username
+      </b-col>
+    </b-row>
+    <b-row align-v="center">
+      <b-col sm="3">이메일</b-col>
+      <b-col sm="9">
+        user.email
+      </b-col>
+    </b-row>
+    <b-row align-v="center">
+      <b-col sm="3">비밀번호</b-col>
+      <b-col sm="9">
+        <b-form-input type="password" id="password" placeholder="비밀번호 변경 시 입력하세요." aria-describedby="password-feedback" v-model="password" :state="passwordValid"></b-form-input>
+        <b-form-invalid-feedback id="password-feedback">
+          영문자와 숫자를 포함해 8글자 이상으로 입력하세요. </b-form-invalid-feedback>
+      </b-col>
+    </b-row>
+    <b-row align-v="center">
+      <b-col sm="3">비밀번호 재입력</b-col>
+      <b-col sm="9">
+        <b-form-input type="password" id="password" aria-describedby="password-again-feedback" v-model="passwordAgain" :state="passwordAgainValid"></b-form-input>
+        <b-form-invalid-feedback id="password-again-feedback">
+          비밀번호가 다릅니다.</b-form-invalid-feedback>
+      </b-col>
+    </b-row>
+    <b-row align-v="center">
+      <b-col sm="3">닉네임</b-col>
+      <b-col sm="9">
+        user.nickname
+      </b-col>
+    </b-row>
+    <b-row align-v="center">
+      <b-col sm="3">프로필 사진</b-col>
+      <b-col sm="9">
+        <b-form-file v-model="profileImage" class="mt-3" accept="image/*" plain></b-form-file>
+        <div class="mt-3">Selected file: {{ profileImage ? profileImage.name : '' }}</div>
+      </b-col>
+    </b-row>
+    <b-row align-v="center">
+      <b-col sm="3">소개글</b-col>
+      <b-col sm="9"><b-form-textarea
+      id="textarea" rows="3" max-rows="6"></b-form-textarea>
+      </b-col>
+    </b-row>
+    <b-row align-v="center" align-h="center">
+      <b-col sm="2"></b-col>
+      <b-col sm="4">
+        <b-button type="submit" variant="primary" id="updateButton" to="/userUpdate" block>수정</b-button>
+      </b-col>
+      <b-col sm="4">
+        <b-button variant="danger" id="deleteButton" block>탈퇴</b-button>
+      </b-col>
+      <b-col sm="2"></b-col>
+    </b-row>
+  </b-container>
   </div>
 </template>
 
 <script>
 export default {
     name: 'UserDetail',
-    props: {
-      user: Object
+    computed: {
+      passwordValid() {
+        const len = this.password.length
+        if (len == 0) return null;
+        if (len < 8) return false;
+        var numFlag = false, alphaFlag = false
+        for (var i = 0; i < len; i++) {
+          const ch = this.password[i]
+          if ('0' <= ch && ch <= '9') numFlag = true
+          else if ('a' <= ch && ch <= 'z') alphaFlag = true
+        }
+        return numFlag && alphaFlag
+      },
+      passwordAgainValid() {
+        const len = this.passwordAgain.length
+        if (len == 0) return null;
+        if(len > 0 && this.password == this.passwordAgain) return true;
+        return false;
+      }
+    },
+    data() {
+      return {
+        password: "",
+        passwordAgain: "",
+        profileImage: ""
+      }
     }
 }
 </script>
 
 <style>
-
 </style>
