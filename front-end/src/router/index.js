ada179c6315c1f9f7a632c9b3ebe12030dd94886
EunJeong <kkwak96@gmail.com>
2020-07-21 15:48:45 +0900

    0721 | Update router

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import UserInfoView from '../views/UserInfoView.vue'
+import PasswordAuthView from '..views/PasswordAuthView.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +11,16 @@ Vue.use(VueRouter)
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/userInfo',
+    name: 'UserInfoView',
+    component: UserInfoView
+  },
+  {
+    path: '/passwordAuth',
+    name: 'PasswordAuthView',
+    component: PasswordAuthView
   },
 ]
 
