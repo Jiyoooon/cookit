685ac65c107705ed5919e1c26d7aba923acd5e9c
EunJeong <kkwak96@gmail.com>
2020-07-21 20:57:11 +0900

    0721 | Update App.vue for router-link

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <router-link to="/userInfo">userInfo</router-link>
+    <router-link to="/Signup">회원가입</router-link>&nbsp;
+    <router-link to="/Login">로그인</router-link>&nbsp;
+    <router-link to="/PasswordAuth">회원정보 수정</router-link>&nbsp;
+    <router-link to="/Logout">로그아웃</router-link>&nbsp;
     <router-view></router-view>
   </div>
 </template>
