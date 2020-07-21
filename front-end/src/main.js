ea1c26e6ca680625b3537bb3fb6d08c8cd421927
EunJeong <kkwak96@gmail.com>
2020-07-21 16:11:52 +0900

    0721 | Add Vuetify

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -3,12 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
 
