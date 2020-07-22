ea5439d55bb5f4f8f32f5d10d1b6efb1dca8711d
kimtaehyeong93 <bobosara@naver.com>
2020-07-22 11:23:54 +0900

    0722 | Completed Login, Logout logic

--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -1,20 +1,39 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import router from '../router'
+import axios from 'axios'
+import cookies from 'vue-cookies'
+
 
 Vue.use(Vuex)
 
 const moduleAccounts = {
   namespaced: true,
   state: {
-
+    authToken: cookies.get('auth-token'),
   },
   getters: {
-
+    IsLoggined(state) {
+      if (state.authToken) {
+        return true
+      } else {
+        return false
+      }
+    },
+    config(state) {
+      return {
+        headers: {
+          Authoriaztion: `Token ${state.authToken}`
+        }
+      }
+    },
   },
 
   mutations: {
-
+     SET_TOKEN(state, token) {
+       state.authToken = token
+       cookies.set('auth-token', token)
+     },
   },
 
   actions: {
@@ -22,8 +41,28 @@ const moduleAccounts = {
       console.log('!!!')
       router.push({ name: 'UserInfoView' })
     },
+
     RedirectAfterUserDelete() {
       router.push({ name: 'UserInfoView' })
+    },
+
+    login({ commit }, loginData) {
+      axios.post('loginurl', loginData)
+        .then((res) => {
+          console.log(res.data)
+          commit('SET_TOKEN', res.data)
+          router.push({ name: '첫화면'})
+        })
+        .catch( err => console.log(err.response))
+    },
+
+    logout() {
+      axios.post('logouturl', null, state.config)
+        .then(res => {
+          console.log(res.data)
+          router.push({ name: '첫화면'})
+        })
+        .catch(err => console.log(err.response))
     },
   },
 }
