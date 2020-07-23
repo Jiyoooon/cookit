78a17fdc904958b666203a4c14cac94e94eaeca6
kimtaehyeong93 <bobosara@naver.com>
2020-07-23 17:01:45 +0900

    0723 | Create URL.js

---
+++ front-end/src/api/url.js
@@ -0,0 +1,23 @@
+export default {
+    URL: 'http://i3a201.p.ssafy.io:8080/cooking-0.0.1-SNAPSHOT/v2/api-docs',
+    ROUTES: {
+        accounts: {
+            signup: '/user/join',
+            login: '/user/login',
+            logout: '/user/logout',
+            baseuser: '/user/',
+            getcomments: '/user/comments/',
+            checkemail: '/user/dup/email/',
+            checknickname: '/user/dup/nickname/',
+            getfollowers: '/user/followers/',
+            checkkey: '/user/verification/check/',
+            requestkey: '/user/verification/send/',
+        },
+        info: {
+
+        },
+        recipe: {
+
+        },
+    }
+}
