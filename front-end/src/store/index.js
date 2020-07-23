eb0b154c5f2175fdd280139136ebda749074f743
EunJeong <kkwak96@gmail.com>
2020-07-23 13:49:22 +0900

    0723 | Connect authorization API #S03P12A201-67

--- front-end/src/store/index.js
+++ front-end/src/store/index.js
@@ -1,19 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import router from '../router'
-// import axios from 'axios'
+import axios from 'axios'
 import cookies from 'vue-cookies'
 
 
 Vue.use(Vuex)
-
+axios.defaults.baseURL = 'http://6d04d76f2aaa.ngrok.io';
 const moduleAccounts = {
   namespaced: true,
   state: {
     authToken: cookies.get('auth-token'),
+    
   },
   getters: {
-    IsLoggined(state) {
+    IsLoggedIn(state) {
       if (state.authToken) {
         return true
       } else {
@@ -46,15 +47,15 @@ const moduleAccounts = {
       router.push({ name: 'UserInfoView' })
     },
 
-    // login({ commit }, loginData) {
-    //   axios.post('loginurl', loginData)
-    //     .then((res) => {
-    //       console.log(res.data)
-    //       commit('SET_TOKEN', res.data)
-    //       router.push({ name: '첫화면'})
-    //     })
-    //     .catch( err => console.log(err.response))
-    // },
+    login({ commit }, loginData) {
+      axios.post('loginurl', loginData)
+        .then((res) => {
+          console.log(res.data)
+          commit('SET_TOKEN', res.data)
+          router.push({ name: '첫화면'})
+        })
+        .catch( err => console.log(err.response))
+    },
 
     // logout() {
     //   axios.post('logouturl', null, state.config)
@@ -64,6 +65,26 @@ const moduleAccounts = {
     //     })
     //     .catch(err => console.log(err.response))
     // },
+
+    emailAuthCodeSend(context, email) {
+      console.log(email)
+      axios.get(`/user/verification/send/${String(email)}`)
+      .then(res => {
+        console.log(`Code send: ${String(res.data.result)}`)
+      })
+      .catch(err => console.log(err.response))
+    },
+
+    emailAuthCodeCheck(context, code) {
+      console.log(code)
+      axios.get(`/user/verification/check/${String(code)}`)
+      .then(res => {
+        console.log(`Code check: ${String(res.data.result)}`)
+        if (res.data.result == 'success') router.push({name: 'SignUp'})
+        else if (res.data.result == 'fail') alert("코드가 맞지 않습니다.")
+      })
+      .catch(err => console.log(err.response))
+    }
   },
 }
 
