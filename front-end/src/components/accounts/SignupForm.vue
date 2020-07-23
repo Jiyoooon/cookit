1cdae46921468d80564c2ff4f617c27fc1e8bcf6
차보람 <ckqhfka4520@naver.com>
2020-07-23 12:39:22 +0900

    0723 | Update SignupForm

--- front-end/src/components/accounts/SignupForm.vue
+++ front-end/src/components/accounts/SignupForm.vue
@@ -5,30 +5,21 @@
         <b-col sm="3">
           <label for="input-userid">아이디<span style="color: red">*</span></label>
         </b-col>
+        <!-- 아이디 대신 이메일~--> 
         <b-col sm="9">
-          <b-form-input 
-            id="input-userid" 
-            v-model="userid"
-            :state="useridState"
-            aria-describedby="input-userid-help input-userid-feedback"
-            placeholder="아이디 입력 (최소 8글자, 영어만 사용가능)"
-            trim
-            >
-          </b-form-input>
-          <b-form-invalid-feedback id="userid-feedback" >
-            아이디가 올바르지 않습니다.(최소 8글자, 영어만 사용가능)
-          </b-form-invalid-feedback>
+          이메일
         </b-col>
       </b-row>
 
+      <!-- 최대 12 -->
       <b-row align-v="center">
         <b-col sm="3">
           <label for="input-none">비밀번호<span style="color: red">*</span></label>
         </b-col>
         <b-col sm="9">
-          <b-form-input type="password" id="password" aria-describedby="password-feedback" v-model="password" :state="passwordValid" placeholder="비밀번호 (최소 8글자,영문과 숫자를 포함)"></b-form-input>
+          <b-form-input type="password" id="password" aria-describedby="password-feedback" v-model="signupData.Password" :state="passwordValid" ></b-form-input>
           <b-form-invalid-feedback id="password-feedback">
-          영문자와 숫자를 포함해 8글자 이상으로 입력하세요. </b-form-invalid-feedback>
+          영문과 숫자를 포함해 8글자 이상으로 입력하세요. </b-form-invalid-feedback>
         </b-col>
       </b-row>
 
@@ -37,12 +28,13 @@
           <label for="input-none">비밀번호 재입력<span style="color: red">*</span></label>
         </b-col>
         <b-col sm="9">
-          <b-form-input type="password" id="password" aria-describedby="password-again-feedback" v-model="passwordAgain" :state="passwordAgainValid" placeholder="비밀번호 확인"></b-form-input>
+          <b-form-input type="password" id="password" aria-describedby="password-again-feedback" v-model="signupData.PasswordAgain" :state="passwordAgainValid" ></b-form-input>
           <b-form-invalid-feedback id="password-again-feedback">
           비밀번호가 다릅니다.</b-form-invalid-feedback>
         </b-col>
       </b-row>
       
+      <!--중복 불가능, 3글자 이상, 특수문자 x 최대 7글자 , -->
       <b-row align-v="center">
         <b-col sm="3">
           <label for="input-userid">닉네임<span style="color: red">*</span></label>
@@ -50,15 +42,14 @@
         <b-col sm="9">
           <b-form-input 
             id="input-userid" 
-            v-model="nickName"
-            :state="NickNameState"
+            v-model="signupData.NickName"
+            :state="NickNameinValid"
             aria-describedby="input-userid-help input-userid-feedback"
-            placeholder="사용할 별명을 적어주세요 (최소2글자, 특수문자, 공백 사용불가)"
-            trim
+            toLowerCase
             >
           </b-form-input>
           <b-form-invalid-feedback>
-            닉네임이 올바르지 않습니다.(최소2글자, 특수문자, 공백 사용불가)
+            닉네임이 올바르지 않습니다.(4~12글자 한글은 최대 6글자)
           </b-form-invalid-feedback>
         </b-col>
       </b-row>
@@ -69,8 +60,8 @@
           프로필사진
         </b-col>
         <b-col sm="9">
-          <b-form-file v-model="file2" class="mt-3" plain></b-form-file>
-          <div class="mt-3">Selected file: {{ file2 ? file2.name : '' }}</div>
+          <b-form-file v-model="signupData.File2" class="mt-3" plain></b-form-file>
+          <div class="mt-3">Selected file: {{ signupData.File2 ? signupData.File2.name : '' }}</div>
         </b-col>
       </b-row>
       <b-row align-v="center">
@@ -104,53 +95,72 @@
 
 <script>
   export default {
+    name:'signup',
     data() {
       return {
-        userid: '',
-        password: '',
-        passwordAgain:'',
-        nickName:'',
-        file:null,
-        file2:null,
+        signupData:{
+          Id: '',
+          Password: '',
+          PasswordAgain:'',
+          NickName:'',
+          File:null,
+          File2:null,
+        }
       }
     },
     computed: {
-      useridState() {
-        if(this.userid.length==0){
-          return null
-        }
-        return this.userid.length > 7 ? true : false
-      },
-      NickNameState() {
-        if(this.nickName.length==0){
-          return null
-        }
-        return this.nickName.length > 1 ? true : false
-      },
+      //패스워드 유효성
+      //3~12 영어와 숫자만가능
       passwordValid() {
-        if(this.password.length==0){
+        const len = this.signupData.Password.length
+        if(len==0){
           return null
         }
-        const len = this.password.length
-        if (len < 8) return false;
-        var numFlag = false, alphaFlag = false
+        if (len < 8 || len >12) return false;
+        var numFlag = false, alphaFlag = false , otherFlag =true
         for (var i = 0; i < len; i++) {
-          const ch = this.password[i]
+          const ch = this.signupData.Password[i]
           if ('0' <= ch && ch <= '9') numFlag = true
-          else if ('a' <= ch && ch <= 'z') alphaFlag = true
+          else if ('a' <= ch && ch <= 'z') {
+            alphaFlag = true
+          }
+          else otherFlag = false
         }
-        return numFlag && alphaFlag
+        return numFlag && alphaFlag &&otherFlag
       },
       passwordAgainValid() {
-        if(this.passwordAgain.length==0){
+        if(this.signupData.PasswordAgain.length==0){
           return null
         }
-        if(this.passwordAgain.length > 0 && this.password == this.passwordAgain) return true;
+        if(this.signupData.PasswordAgain.length > 0 && this.signupData.Password == this.signupData.PasswordAgain) return true;
         return false;
       },
+      NickNameinValid(){
+        const len = this.signupData.NickName.length
+        if(len==0){
+          return null
+        }
+        var NickNamelen = 0
+        var nameflag = true
+        for (var i = 0; i < len; i++) {
+          const ch = this.signupData.NickName[i]
+          console.log("ch: " + ch)
+          if(('0' <= ch && ch <= '9') || ('a' <= ch && ch <= 'z')||('A' <= ch && ch <= 'Z')){
+            NickNamelen++
+          }
+          else if(('가' <= ch && ch <= '힣')){
+            NickNamelen+=2
+          }
+          else{
+            nameflag = false
+          }
+        }
+        if(NickNamelen <4 || NickNamelen >12){
+          return false
+        }
+        return nameflag
+      },
     },
-    
-    
   }
 </script>
 
