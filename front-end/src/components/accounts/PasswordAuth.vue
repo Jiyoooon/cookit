4e5e1fe2e1f8bb47bb42d0f5ae9544a2c2354f06
차보람 <ckqhfka4520@naver.com>
2020-07-25 15:59:41 +0900

    0725 | Add PasswordAuth.vue

--- front-end/src/components/accounts/PasswordAuth.vue
+++ front-end/src/components/accounts/PasswordAuth.vue
@@ -4,10 +4,10 @@
     <b-row align-v="center">
         <b-col sm="2"></b-col>
       <b-col sm="6">
-        <b-form-input type="password" id="password" aria-describedby="password-feedback" placeholder="비밀번호를 입력하세요."></b-form-input>
+        <b-form-input v-model='passwordData.password' type="password" id="password" aria-describedby="password-feedback" placeholder="비밀번호를 입력하세요."></b-form-input>
       </b-col>
       <b-col sm="2">
-        <b-button @click="passwordCheck" type="submit" variant="primary" id="okButton"  block>확인</b-button>
+        <b-button @click="passwordCheck(passwordData)" type="submit" variant="primary" id="okButton"  block>확인</b-button>
       </b-col>
       <b-col sm="2"></b-col>
     </b-row>
@@ -21,6 +21,13 @@ import { mapActions } from 'vuex'
 
 export default {
   name: 'PasswordAuth',
+  data(){
+    return{
+      passwordData:{
+        password:''
+      }
+    }
+  },
   methods:{
     // 버튼을 누르면 api이용 비밀번호일치여부를 받아야함 => action
     ...mapActions('accounts',[
