182b6da2cfcb00e2f0dcc94b29595459ef8fbd24
EunJeong <kkwak96@gmail.com>
2020-07-26 21:48:00 +0900

    0726 | Make new components

---
+++ front-end/src/components/editor/CookingStep.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: 'CookingStep',
+}
+</script>
+
+<style>
+
+</style>
---
+++ front-end/src/components/editor/Ingredient.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+export default {
+    name: 'Ingredients',
+}
+</script>
+
+<style>
+
+</style>
