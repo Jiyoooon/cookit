1b14f4f9b629711f3b879ceb7e4c6426af17f7f8
차보람 <ckqhfka4520@naver.com>
2020-07-27 14:02:39 +0900

    0727 | Update to use bootstrapicon

--- front-end/src/plugins/bootstrap-vue.js
+++ front-end/src/plugins/bootstrap-vue.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 
-//import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-Vue.use(BootstrapVue)
+import { BootstrapVue, IconsPlugin, BootstrapVueIcons } from 'bootstrap-vue'
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(BootstrapVueIcons)
