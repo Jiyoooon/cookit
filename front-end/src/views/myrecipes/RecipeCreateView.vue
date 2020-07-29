09095bb9ed80533afc57c7a7998a80c21ee606c0
EunJeong <kkwak96@gmail.com>
2020-07-29 13:46:31 +0900

    0729 | Update RecipeCreateView.vue

---
+++ front-end/src/views/myrecipes/RecipeCreateView.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="recipePage">
+    <div id="recipeInfo">
+      <h2>레시피 정보</h2>
+      <hr>
+      <recipeInfo></recipeInfo>
+    </div>
+    <div id="ingredients">
+      <h2>재료</h2>
+      <hr>
+      <b-container fluid="lg">
+        <b-row>
+          <b-col class="mt-3">주재료</b-col>
+          <b-col sm="5"><ingredient></ingredient></b-col>
+          <b-col class="mt-3">부재료</b-col>
+          <b-col sm="5"><ingredient></ingredient></b-col>
+        </b-row>
+      </b-container>
+    </div>
+    <div id="steps">
+      <h2>조리 과정</h2>
+      <hr>
+      <cooking-step></cooking-step>
+    </div>
+  </div>
+</template>
+
+<script>
+import RecipeInfo from '@/components/editor/RecipeInfo.vue'
+import Ingredient from '@/components/editor/Ingredient.vue'
+import CookingStep from '@/components/editor/CookingStep.vue'
+
+export default {
+    name: 'RecipeCreateView',
+    components: {
+      RecipeInfo, Ingredient, CookingStep
+    },
+    data() {
+      return {
+        items: [],
+      }
+    },
+    methods: {
+      addItem() {
+
+      },
+      deleteItem() {
+
+      }
+    }
+}
+</script>
+
+<style>
+#recipePage {
+  width: 80%;
+  display: block;
+  margin: 0px auto;
+}
+</style>
