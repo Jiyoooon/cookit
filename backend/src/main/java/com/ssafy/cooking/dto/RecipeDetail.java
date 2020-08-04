af6eb5f7e7f28c7dd1490be6f6d9095ac1a9b834
multicampus <multicampus@DESKTOP-KVCQHCD.mshome.net>
2020-08-04 14:49:34 +0900

    0804 | update getRecipeById

--- backend/src/main/java/com/ssafy/cooking/dto/RecipeDetail.java
+++ backend/src/main/java/com/ssafy/cooking/dto/RecipeDetail.java
@@ -5,6 +5,24 @@ import java.util.List;
 public class RecipeDetail extends Recipe {
 	private List<Ingredient> ingredients;
 	private List<CookingStep> cookingStep;
+	
+	public RecipeDetail(Recipe recipe) {
+		setRecipe_id(recipe.getRecipe_id());
+		setTitle(recipe.getTitle());
+		setDescription(recipe.getDescription());
+		setRecipe_user(recipe.getRecipe_user());
+		setRecipe_user_name(recipe.getRecipe_user_name());
+		setCategory_id(recipe.getCategory_id());
+		setMain_image(recipe.getMain_image());
+		setServings(recipe.getServings());
+		setCooking_time(recipe.getCooking_time());
+		setLevel(recipe.getLevel());
+		setHits(recipe.getHits());
+		setTag(recipe.getTag());
+		setCreate_date(recipe.getCreate_date());
+		setUpdate_date(recipe.getUpdate_date());
+		setDelete_date(recipe.getDelete_date());
+	}
 
 	public List<Ingredient> getIngredients() {
 		return ingredients;
