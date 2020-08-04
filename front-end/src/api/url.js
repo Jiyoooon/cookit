33efe1aa0f6e9a161fa48b6a55b794f9d6c9c794
kimtaehyeong93 <bobosara@naver.com>
2020-08-04 14:49:20 +0900

    0804 | Update RecipeDetailView

--- front-end/src/api/url.js
+++ front-end/src/api/url.js
@@ -1,15 +1,16 @@
 export default {
-    URL: 'http://i3a201.p.ssafy.io:8080/cooking-0.0.2-SNAPSHOT/',
+    URL: 'http://i3a201.p.ssafy.io:8080/cooking-0.0.1-SNAPSHOT/',
     ROUTES: {
         accounts: {
             signup: '/user/join',
             login: '/user/login',
-            logout: '/user/logout',
-            baseuser: '/user',
+            logout: '/user/token/logout',
+            baseuser: '/user/token',
+            fetchuserinfo: '/user/',
             getcomments: '/user/comments/',
             checkemail: '/user/dup/email/',
             checknickname: '/user/dup/nickname/',
-            checkpassword: '/user/password',
+            checkpassword: '/user/token/password',
             getfollowers: '/user/followers/',
             checkkey: '/user/verification/check',
             requestkey: '/user/verification/send/',
@@ -26,6 +27,9 @@ export default {
         lookaroundrecipe:{
             getfilteredrecipes: '/recipe/recipes',
             getingredients: '/recipe/ingredients',
+        },
+        recipeview: {
+            fetchrecipe: '/recipe/'
         },
     }
 }
