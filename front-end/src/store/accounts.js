d73901d2440f5b9b8bcc15cefe20b7bbf003217c
EunJeong <kkwak96@gmail.com>
2020-08-09 00:51:25 +0900

    0809 | Seperate store/index.js by modules

---
+++ front-end/src/store/accounts.js
@@ -0,0 +1,479 @@
+import router from '../router'
+import axios from 'axios'
+import cookies from 'vue-cookies'
+import SERVER from '../api/url.js'
+
+export default {
+  namespaced: true,
+  state: {
+    authToken: cookies.get('auth-token'),
+    authUser: cookies.get('auth-user'),
+    userEmail: cookies.get('user-email'),
+    validEmail: false,
+    updateTF: false,
+  },
+
+  getters: {
+    isLoggedIn(state) {
+      if (state.authToken) {
+        return true
+      } else {
+        return false
+      }
+    },
+    config(state) {
+      return {
+        headers: {
+          Authorization: `token ${state.authToken}`
+        }
+      }
+    },
+    isValidEmail(state) {
+      return state.validEmail
+    }
+  },
+
+  mutations: {
+     SET_TOKEN(state, token) {
+       state.authToken = token
+       cookies.set('auth-token', token)
+     },
+
+     SET_EMAIL(state, email) {
+       state.userEmail = email
+       cookies.set('user-email', email)
+     },
+     SET_USER(state, user) {
+       state.authUser = user
+       cookies.set('auth-user', user)
+     },
+     SET_VALID(state, data) {
+       state.validEmail = data
+     },
+     SET_UPDATETF(state, value) {
+       console.log('fsdfsf')
+       state.updateTF = value
+     },
+  },
+
+  actions: {
+    DeleteUser({ commit, getters }) {
+      this._vm.$root.$bvModal.msgBoxConfirm('한번 삭제된 데이터는 복구되지 않습니다.', {
+        title: '정말로 탈퇴하시겠습니까?',
+        size: 'lg',
+        buttonSize: 'sm',
+        okVariant: 'danger',
+        okTitle: 'YES',
+        cancelTitle: 'NO',
+        footerClass: 'p-2',
+        hideHeaderClose: false,
+        centered: true
+      })
+        .then((ans) => {
+          if (ans) {
+            axios.delete(SERVER.ROUTES.accounts.baseuser, getters.config)
+              .then((res) => {
+                if(res.data.result == 'success') {
+                  this._vm.$root.$bvModal.msgBoxOk('회원탈퇴 되었습니다.', {
+                    title: 'Confirmation',
+                    size: 'sm',
+                    buttonSize: 'sm',
+                    okVariant: 'success',
+                    headerClass: 'p-2 border-bottom-0',
+                    footerClass: 'p-2 border-top-0',
+                    centered: true
+                  })
+                  commit('SET_TOKEN', null)
+                  commit('SET_USER', null)
+                  commit('SET_EMAIL', null)
+                  commit('myblog/SET_RECIPES', null, { root: true })
+                  cookies.remove('auth-token')
+                  cookies.remove('auth-user')
+                  cookies.remove('user-email')
+                  router.push({ name: 'LookAroundRecipeView' })
+                } else {
+                  this._vm.$root.$bvModal.msgBoxOk('실패하였습니다.', {
+                    title: 'Confirmation',
+                    size: 'sm',
+                    buttonSize: 'sm',
+                    okVariant: 'danger',
+                    headerClass: 'p-2 border-bottom-0',
+                    footerClass: 'p-2 border-top-0',
+                    centered: true
+                  })
+                }
+              })
+              .catch((err) => {
+                console.log(err.response)
+                alert(err.response)
+              })
+          }
+        })
+        .catch(err => {
+          console.log(err.response)
+          alert(err.response)
+        })
+    },
+
+    GoHome() {
+      router.push({ name: 'Home'})
+    },
+
+    GoSignup() {
+      router.push({ name: 'Signup'})
+    },
+
+    GoLogin() {
+      router.push({ name: 'Login'})
+    },
+
+    GoLogout() {
+      router.push({ name: 'Logout'})
+    },
+
+    GoUserInfo({ commit }) {
+      router.push({ name: 'UserInfoView'})
+      commit('SET_UPDATETF', false)
+    },
+
+    GoEmailAuth() {
+      router.push({ name: 'EmailAuthView'})
+    },
+    GoPasswordAuth(){
+      router.push({ name: 'PasswordAuthView'})
+    },
+    GoPasswordFind(){
+      router.push({ name: 'PasswordFindView'})
+    },
+    GoRecipeCreate(){
+      router.push({ name: 'RecipeCreateView'})
+    },
+    RedirectAfterUserUpdate() {
+      router.push({ name: 'UserInfoView' })
+    },
+
+    RedirectAfterUserDelete() {
+      router.push({ name: 'UserInfoView' })
+    },
+
+    login({ commit, dispatch }, loginData) {
+      axios.post(SERVER.ROUTES.accounts.login, loginData)
+        .then((res) => {
+          if (res.data.result == 'success') {
+            commit('SET_TOKEN', res.headers.token)
+            dispatch('fetchUser')
+            dispatch('myblog/fetchMyRecipes', null, { root: true })
+            // dispatch('GoHome')
+            router.push({ name: 'LookAroundRecipeView'})
+          } else {
+            this._vm.$root.$bvModal.msgBoxOk('이메일과 비밀번호를 확인하여 주십시오.', {
+              title: 'Confirmation',
+              size: 'sm',
+              buttonSize: 'sm',
+              okVariant: 'danger',
+              headerClass: 'p-2 border-bottom-0',
+              footerClass: 'p-2 border-top-0',
+              centered: true
+            })
+          }
+        })
+        .catch((err) => {
+          console.log(err.response)
+          alert("!!!!")
+        })
+    },
+
+    logout({ commit, getters }) {
+      axios.get(SERVER.ROUTES.accounts.logout, getters.config)
+        .then(() => {
+          commit('SET_TOKEN', null)
+          commit('SET_USER', null)
+          commit('SET_EMAIL', null)
+          commit('myblog/SET_RECIPES', null, { root: true })
+          cookies.remove('auth-token')
+          cookies.remove('auth-user')
+          cookies.remove('user-email')
+          router.push({ name: 'LookAroundRecipeView'})
+        })
+        .catch(err => {
+          console.log(err.response)
+          alert(err.response.data.cause)
+        })
+    },
+
+    emailDupCheck({commit}, email) {
+      axios.get(`/user/dup/email/${String(email)}`)
+      .then(res => {
+        if (res.data.result == 'success')
+          commit('SET_VALID', true)
+        else if (res.data.result == 'fail')
+          commit('SET_VALID', false)
+      })
+    },
+
+    emailAuthCodeSend({ commit }, email) {
+      axios.get(SERVER.ROUTES.accounts.requestkey + String(email))
+      .then(res => {
+        if(res.data.result == 'success') {
+          commit('SET_EMAIL', email)
+          this._vm.$root.$bvModal.msgBoxOk('인증 코드가 발송되었습니다.', {
+            title: 'Confirmation',
+            size: 'sm',
+            buttonSize: 'sm',
+            okVariant: 'success',
+            headerClass: 'p-2 border-bottom-0',
+            footerClass: 'p-2 border-top-0',
+            centered: true
+          })
+        } else {
+          this._vm.$root.$bvModal.msgBoxOk('인증 코드 발송에 실패하였습니다.', {
+            title: 'Confirmation',
+            size: 'sm',
+            buttonSize: 'sm',
+            okVariant: 'danger',
+            headerClass: 'p-2 border-bottom-0',
+            footerClass: 'p-2 border-top-0',
+            centered: true
+          })
+        }
+      })
+      .catch(err => {
+        console.log(err.response)
+        alert(err.response)
+      })
+    },
+
+    emailAuthCodeCheck({ commit, state, dispatch }, code) {
+      const emailConfirm = {
+        email: state.userEmail,
+        code: code,
+      }
+      axios.post(SERVER.ROUTES.accounts.checkkey, emailConfirm)
+      .then(res => {
+        if (res.data.result == 'success') dispatch('GoSignup')
+        else if (res.data.result == 'fail') {
+          this._vm.$root.$bvModal.msgBoxOk('코드가 맞지 않습니다.', {
+            title: 'Confirmation',
+            size: 'sm',
+            buttonSize: 'sm',
+            okVariant: 'danger',
+            headerClass: 'p-2 border-bottom-0',
+            footerClass: 'p-2 border-top-0',
+            centered: true
+          })
+          commit('SET_EMAIL', null)
+        }
+      })
+      .catch(err => {
+        console.log(err.response)
+        alert('!!!!!!')
+       })
+    },
+    fetchUser({ getters, commit }) {
+      axios.get(SERVER.ROUTES.accounts.baseuser, getters.config)
+        .then((res) => {
+          commit('SET_USER', res.data.data)
+        })
+        .catch((err) => {
+          console.err(err.response)
+          alert(err.response)
+        })
+    },
+    updateUser({ dispatch, state, commit }, updateData) {
+      if (!updateData.valid.password) {
+        this._vm.$root.$bvModal.msgBoxOk('비밀번호가 일치하지 않습니다.', {
+          title: 'Confirmation',
+          size: 'sm',
+          buttonSize: 'sm',
+          okVariant: 'danger',
+          headerClass: 'p-2 border-bottom-0',
+          footerClass: 'p-2 border-top-0',
+          centered: true
+        })
+      } else if (!updateData.valid.nickname) {
+        this._vm.$root.$bvModal.msgBoxOk('닉네임 중복체크를 해주세요.', {
+          title: 'Confirmation',
+          size: 'sm',
+          buttonSize: 'sm',
+          okVariant: 'danger',
+          headerClass: 'p-2 border-bottom-0',
+          footerClass: 'p-2 border-top-0',
+          centered: true
+        })
+      } else {
+        const formData = new FormData()
+        formData.append('email', updateData.config.email)
+        formData.append('password', updateData.config.password)
+        formData.append('nickname', updateData.config.nickname)
+        formData.append('profile', updateData.config.profile)
+        formData.append('image_name', updateData.config.image_name)
+        formData.append('intro', updateData.config.intro)
+        formData.append('start_page', updateData.config.start_page)
+
+        // for (let [key, value] of formData.entries()) {
+        //   console.log(`${key} : ${value}`)
+        //   if (key == 'profile') {
+        //     console.log(value)
+        //   }
+        // }
+
+        const headerconfig = { headers: {
+          'Authorization': `token ${state.authToken}`,
+          'Content-Type': 'multipart/form-data'
+        }}
+
+        axios.put(SERVER.ROUTES.accounts.baseuser, formData, headerconfig)
+          .then((res) => {
+            if (res.data.result == 'success') {
+              this._vm.$root.$bvModal.msgBoxOk('수정되었습니다.', {
+                title: 'Confirmation',
+                size: 'sm',
+                buttonSize: 'sm',
+                okVariant: 'success',
+                headerClass: 'p-2 border-bottom-0',
+                footerClass: 'p-2 border-top-0',
+                centered: true
+              })
+            dispatch('fetchUser')
+            commit('SET_UPDATETF', false)
+            }
+          })
+          .catch((err) => {
+            console.log(err.response)
+            alert(err.response.data.error)
+          })
+      }
+    },
+    passwordCheck({ dispatch, getters } ,password) {
+      axios.post(SERVER.ROUTES.accounts.checkpassword, password, getters.config)
+      .then((res) => {
+        if(res.data.result == 'success') {
+          cookies.set('password-check', 1)
+          this._vm.$root.$bvModal.msgBoxOk('확인되었습니다.', {
+            title: 'Confirmation',
+            size: 'sm',
+            buttonSize: 'sm',
+            okVariant: 'success',
+            headerClass: 'p-2 border-bottom-0',
+            footerClass: 'p-2 border-top-0',
+            centered: true
+          })
+          .then((ans) => {
+            if (ans) {
+              dispatch('GoUserInfo')
+            }
+            cookies.remove('password-check')
+          })
+          dispatch('GoUserInfo')
+        } else {
+          alert("비밀번호가 일치하지 않습니다.")
+        }
+      })
+      .catch((err) => {
+        console.log(err.response)
+        alert(err.response)
+      })
+    },
+    sendNewPassword({ dispatch }, email) {
+      axios.get(SERVER.ROUTES.accounts.sendnewpassword + String(email))
+      .then((res) => {
+        if(res.data.result == 'success'){
+          this._vm.$root.$bvModal.msgBoxOk('새 비밀번호가 전송되었습니다', {
+            title: 'Confirmation',
+            size: 'sm',
+            buttonSize: 'sm',
+            okVariant: 'success',
+            headerClass: 'p-2 border-bottom-0',
+            footerClass: 'p-2 border-top-0',
+            centered: true
+          }) 
+          dispatch('GoLogin')
+        } else {
+          this._vm.$root.$bvModal.msgBoxOk('존재하지 않는 이메일입니다.', {
+            title: 'Confirmation',
+            size: 'sm',
+            buttonSize: 'sm',
+            okVariant: 'danger',
+            headerClass: 'p-2 border-bottom-0',
+            footerClass: 'p-2 border-top-0',
+            centered: true
+          }) 
+        }
+      })
+      .catch((err) => {
+        console.log(err.response)
+        alert(err.response)
+      })
+    },
+    signup({ commit, dispatch }, signupData) {
+      if (!signupData.valid.password) {
+        this._vm.$root.$bvModal.msgBoxOk('비밀번호를 확인해주세요.', {
+          title: 'Confirmation',
+          size: 'sm',
+          buttonSize: 'sm',
+          okVariant: 'danger',
+          headerClass: 'p-2 border-bottom-0',
+          footerClass: 'p-2 border-top-0',
+          centered: true
+        })
+      } else if (!signupData.valid.nickname) {
+        this._vm.$root.$bvModal.msgBoxOk('닉네임 중복체크를 해주세요.', {
+          title: 'Confirmation',
+          size: 'sm',
+          buttonSize: 'sm',
+          okVariant: 'danger',
+          headerClass: 'p-2 border-bottom-0',
+          footerClass: 'p-2 border-top-0',
+          centered: true
+        })
+      } else {
+        
+        const formData = new FormData()
+        formData.append('email', signupData.config.email)
+        formData.append('password', signupData.config.password)
+        formData.append('nickname', signupData.config.nickname)
+        formData.append('profile', signupData.config.profile)
+        formData.append('intro', signupData.config.intro)
+        formData.append('image_name', signupData.config.image_name)
+
+        // for (let key of formData.entries()) {
+        //   console.log(`${key}`)
+        // }
+        axios.post(SERVER.ROUTES.accounts.signup, formData, { headers: { 'Content-Type': 'multipart/form-data' }})
+          .then((res) => {
+            if (res.data.result == 'success') {
+              commit('SET_TOKEN', res.headers.token)
+              dispatch('fetchUser')
+              dispatch('myblog/fetchMyRecipes', null, { root: true })
+              this._vm.$root.$bvModal.msgBoxOk('가입되었습니다.', {
+                title: 'Confirmation',
+                size: 'sm',
+                buttonSize: 'sm',
+                okVariant: 'success',
+                headerClass: 'p-2 border-bottom-0',
+                footerClass: 'p-2 border-top-0',
+                centered: true
+              })
+                .then(() => {
+                  router.push({ name: 'LookAroundRecipeView'})
+                })
+            } else {
+              this._vm.$root.$bvModal.msgBoxOk('이미지 파일이 올바르지 않습니다.', {
+                title: 'Confirmation',
+                size: 'sm',
+                buttonSize: 'sm',
+                okVariant: 'danger',
+                headerClass: 'p-2 border-bottom-0',
+                footerClass: 'p-2 border-top-0',
+                centered: true
+              })
+            }
+          })
+          .catch((err) => {
+            console.log(err.response)
+            alert(err.response)
+          })
+        }
+    },
+  },
+}
