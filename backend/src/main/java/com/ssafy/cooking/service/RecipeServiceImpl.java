ded4d1a3f67e40526709d8208608ee7d72625834
multicampus <multicampus@DESKTOP-KVCQHCD.mshome.net>
2020-08-15 11:52:26 +0900

    0815 | update reciperevise

--- backend/src/main/java/com/ssafy/cooking/service/RecipeServiceImpl.java
+++ backend/src/main/java/com/ssafy/cooking/service/RecipeServiceImpl.java
@@ -5,7 +5,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -173,20 +172,25 @@ public class RecipeServiceImpl implements RecipeService {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			recipeData.setMain_image(baseUrl + "/images/recipe/" + imageName + ".jpg");
 		}
-
+		
 		recipeData.setTagString(Arrays.toString(recipeData.getTag()).substring(1, Arrays.toString(recipeData.getTag()).length()-1));
 		
 		int result = recipeDao.reviseRecipe(uid, recipeData);
 		if(result > 0) {
 			int recipe_id = recipeData.getRecipe_id();
-			recipeDao.deleteCookingSteps(recipe_id);
+//			recipeDao.deleteCookingSteps(recipe_id);
 			
 			if (recipeData.getCookingStep() != null) {
 				for (int i = 0; i < recipeData.getCookingStep().size(); i++) {
 					CookingStep step = recipeData.getCookingStep().get(i);
+
 					
-					if (step.getStep_image_file() != null) {
+					if(step.getStep_image() != null && step.getStep_image() != "") {
+						//이미지 경로가 있는 경우에는 수정만 한다.
+						//recipeDao.
+					} else if (step.getStep_image_file() != null) {
 						try {
 							String stepImageName = imageName + Integer.toString(i);
 							writeFile(step.getStep_image_file(), stepImageName, baseUrl);
@@ -197,7 +201,12 @@ public class RecipeServiceImpl implements RecipeService {
 						}
 					}
 				}
-				recipeDao.addCookingsteps(recipe_id, recipeData.getCookingStep());
+				System.out.println(recipe_id);
+				for (CookingStep s : recipeData.getCookingStep()) {
+					System.out.println(s);
+				}
+//				recipeDao.addCookingsteps(recipe_id, recipeData.getCookingStep());
+				recipeDao.reviseCookingsteps(recipe_id, recipeData.getCookingStep());
 			}
 			
 			if (recipeData.getIngredients() != null) {
@@ -261,13 +270,6 @@ public class RecipeServiceImpl implements RecipeService {
 		if(filter.getLike_large() 	!= null) like_large = Arrays.asList(filter.getLike_large().trim().split(","));
 		if(filter.getLike_medium()	!= null) like_medium = Arrays.asList(filter.getLike_medium().trim().split(","));
 		if(filter.getLike_small() 	!= null) like_small = Arrays.asList(filter.getLike_small().trim().split(","));
-		
-//		if(filter.getHate_large() 	!= null) System.out.println(hate_large+", "+hate_large.size());
-//		if(filter.getHate_medium() != null) System.out.println(hate_medium+", "+hate_medium.size());
-//		if(filter.getHate_small() != null) System.out.println(hate_small+", "+hate_small.size());
-//		if(filter.getLike_large() != null) System.out.println(like_large+", "+like_large.size());
-//		if(filter.getLike_medium() != null) System.out.println(like_medium+", "+like_medium.size());
-//		if(filter.getLike_small() != null) System.out.println(like_small+", "+like_small.size());
 		
 		List<Recipe> recipes = recipeDao.getRecipes2(start, end, id, user, query, category, order, likeUser,
 				hate_large, hate_medium, hate_small, like_large, like_medium, like_small);
