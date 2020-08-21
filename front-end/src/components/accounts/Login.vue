d07f885a869cd484270e7f69abd4cfd624b53a42
EunJeong <kkwak96@gmail.com>
2020-08-21 17:20:13 +0900

    0821 | Merge COOKIT

--- front-end/src/components/accounts/Login.vue
+++ front-end/src/components/accounts/Login.vue
@@ -42,7 +42,7 @@ export default {
     computed: {
     },
     methods: {
-        ...mapActions('accounts', ['login', 'GoSignup','GoPasswordFind'])
+        ...mapActions('accounts', ['login', 'GoEmailAuth','GoPasswordFind'])
     }
   }
 
